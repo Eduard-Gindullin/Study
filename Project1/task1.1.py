bfcd46961e70fb67002b4c3056e63e98cfa48024
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2022-12-30 19:26:26 +0500

    v2

--- Project1/task1.1.py
+++ Project1/task1.1.py
@@ -1,10 +1,13 @@
 # Imports
-import sqlite3
+#import sqlite3
 import requests
 import feedparser
 import os
 import urllib
 import random
+import clickhouse_connect
+import pandas as pd
+import numpy as np
 
 
 # Feeds
@@ -35,57 +38,58 @@ proxies = {
 }
 
 # DB
-scriptDir = os.path.dirname(os.path.realpath(__file__))
-db_connection = sqlite3.connect(scriptDir + '/rss.sqlite')
-db = db_connection.cursor()
-db.execute('CREATE TABLE IF NOT EXISTS myrss (title TEXT, date TEXT)')
+
+
+client = clickhouse_connect.get_client(host='localhost', username='default', password='')
+client.command('CREATE TABLE IF NOT EXISTS vedomosti (title String, link String,tags String, published String) ENGINE MergeTree ORDER BY title')
+#scriptDir = os.path.dirname(os.path.realpath(__file__))
+#db_connection = sqlite3.connect(scriptDir + '/rss.sqlite')
+#db = client.cursor()
+#db.execute('CREATE TABLE IF NOT EXISTS vedomosti (title TEXT, date TEXT)')
+
+d = feedparser.parse('https://www.vedomosti.ru/rss/news')
+d['feed']['title']
+data_list = []
+
+for i in d["entries"]:
+    data_list.append([i["title"],i["link"],i["tags"],i["published"]])
+df = pd.DataFrame(data_list, columns=["title","link","tags","published"])
+#df['tags'] = list(df['tags'])
+#df['category'] = []
+#a = df['tags']
+#if a == [{'term': 'Политика', 'scheme': None, 'label': None}]:
+#  df["category"].append('Политика')
+#for i, row in enumerate(df["tags"]):
+# df["category"].append(df["tags"][i][1])
+#for i in df.tags:
+#df['tags'] = df['tags']['term']
+#df['tags'] = df.tags.str.replace("'[{'term': '","', 'scheme': None, 'label'" , '')
+#df['tags'].replace("[{'term': 'Политика', 'scheme': None, 'label': None}]", 'Политика')
+#client.insert('vedomosti', data_list, column_names=["title","link","tags","published"]) 
+client.command(INSERT INTO vedomosti VALUES, df)
+print(df)
+
+
+
 
 # Get posts from DB and print
-def get_posts():
-    with db_connection:
-        db.execute("SELECT * FROM myrss")
-        print(db.fetchall())
+#def get_posts():
+#    with client:
+#        print(client.command("SELECT * FROM vedomosti"))
+
+#article_title = 'privet'
+#article_date = '12.12'   
 
 # Check post in DB
-def article_is_not_db(article_title, article_date):
-    db.execute("SELECT * from myrss WHERE title=? AND date=?", (article_title, article_date))
-    if not db.fetchall():
-        return True
-    else:
-        return False
+#def article_is_not_db(article_title, article_date):
+#    client.command("SELECT * from vedomosti WHERE title=? AND date=?", (article_title, article_date))
+ #   if not db.fetchall():
+ #       return True
+  #  else:
+   #     return False
 
 # Add post to DB
-def add_article_to_db(article_title, article_date):
-    db.execute("INSERT INTO myrss VALUES (?,?)", (article_title, article_date))
-    db_connection.commit()
+#def add_article_to_db(article_title, article_date):
+#    client.command("INSERT INTO vedomosti VALUES (?,?)", (article_title, article_date))
+#    client.execute()
 
-# Send notify to Telegram bot
-def bot_sendtext(bot_message):
-    bot_message = urllib.parse.quote(bot_message)
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-    requests.get(send_text, proxies=proxies, headers=headers)
-    print(send_text)
-
-# Check, read articles
-def read_article_feed(feed):
-    """ Get articles from RSS feed """
-    feedparser.USER_AGENT = ua
-    feed = feedparser.parse(feed)
-    print(feed)
-    for article in feed['entries']:
-        if article_is_not_db(article['title'], article['published']):
-            add_article_to_db(article['title'], article['published'])
-            bot_sendtext('New feed found ' + article['title'] +', ' + article['link'] + ', ' + article['description'])
-            print(article)
-
-# Rotate feeds array
-def spin_feds():
-    for x in myfeeds:
-        print(x)
-        read_article_feed(x)
-
-# Runner :)
-if __name__ == '__main__':
-    spin_feds()
-    # get_posts()
-    db_connection.close()
