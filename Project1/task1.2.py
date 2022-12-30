aca590c98fa453e515f47862ce9cf9b154cea00e
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2022-12-30 13:51:08 +0500

    v1

--- Project1/task1.2.py
+++ Project1/task1.2.py
@@ -1,7 +1,7 @@
 # Imports
 import sqlite3
 import requests
-import feedparser
+#import feedparser
 import os
 import urllib
 import random
@@ -11,3 +11,8 @@ import random
 myfeeds = [
   'https://lenta.ru/rss/',
 ]
+
+import clickhouse_connect
+
+client = clickhouse_connect.get_client(host='localhost', username='default', password='')
+client.command('CREATE TABLE lenta (key UInt32, value String, metric Float64) ENGINE MergeTree ORDER BY key')
