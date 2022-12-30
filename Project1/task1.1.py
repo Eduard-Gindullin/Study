ef6cb18c8877e9884fd7760cdb63ea08fe911d09
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2022-12-30 21:38:37 +0500

    v3

--- Project1/task1.1.py
+++ Project1/task1.1.py
@@ -2,58 +2,34 @@
 #import sqlite3
 import requests
 import feedparser
-import os
-import urllib
+import pandahouse as ph
 import random
 import clickhouse_connect
 import pandas as pd
 import numpy as np
 
 
-# Feeds
-myfeeds = [
-  'https://www.vedomosti.ru/rss/news',
-]
-
-# User agents
-uags = [
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-]
-
-# Random User Agent (from uags list)
-ua = random.choice(uags)
-
-# Header
-headers = {
-  "Connection" : "close",  # another way to cover tracks
-  "User-Agent" : ua
-}
-
-# Proxies
-proxies = {
-}
-
 # DB
 
 
 client = clickhouse_connect.get_client(host='localhost', username='default', password='')
-client.command('CREATE TABLE IF NOT EXISTS vedomosti (title String, link String,tags String, published String) ENGINE MergeTree ORDER BY title')
-#scriptDir = os.path.dirname(os.path.realpath(__file__))
-#db_connection = sqlite3.connect(scriptDir + '/rss.sqlite')
-#db = client.cursor()
-#db.execute('CREATE TABLE IF NOT EXISTS vedomosti (title TEXT, date TEXT)')
+client.command('CREATE TABLE IF NOT EXISTS vedomosti (title String, link String,tags String, published String) ENGINE MergeTree ORDER BY published')
+
 
 d = feedparser.parse('https://www.vedomosti.ru/rss/news')
 d['feed']['title']
 data_list = []
 
 for i in d["entries"]:
-    data_list.append([i["title"],i["link"],i["tags"],i["published"]])
+  
+    data_list.append([i["title"],i["link"],i[d['entries'][i].tags[j].term],i["published"]])
 df = pd.DataFrame(data_list, columns=["title","link","tags","published"])
+connection = dict(database='default',
+                  host='http://localhost:8123',
+                  user='default',
+                  password='')
+
+ph.to_clickhouse(df, 'vedomosti', index=False, chunksize=100000, connection=connection)
 #df['tags'] = list(df['tags'])
 #df['category'] = []
 #a = df['tags']
@@ -66,8 +42,8 @@ df = pd.DataFrame(data_list, columns=["title","link","tags","published"])
 #df['tags'] = df.tags.str.replace("'[{'term': '","', 'scheme': None, 'label'" , '')
 #df['tags'].replace("[{'term': 'Политика', 'scheme': None, 'label': None}]", 'Политика')
 #client.insert('vedomosti', data_list, column_names=["title","link","tags","published"]) 
-client.command(INSERT INTO vedomosti VALUES, df)
-print(df)
+#client.command('INSERT INTO vedomosti VALUES, df')
+#print(df)
 
 
 
