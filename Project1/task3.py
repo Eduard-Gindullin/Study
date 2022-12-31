f412b574a50800f08f904fecd256940963af22b9
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2022-12-31 11:57:57 +0500

    v1

--- Project1/task3.py
+++ Project1/task3.py
@@ -8,3 +8,5 @@ connection = dict(database='default',
                   host='http://localhost:8123',
                   user='default',
                   password='')
+client = clickhouse_connect.get_client(host='localhost', username='default', password='')
+client.command('CREATE TABLE IF NOT EXISTS vedomosti (title String, date String) ENGINE MergeTree ORDER BY title')
