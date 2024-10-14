9a019d93b3182be1ba19dc7e8be5851c5ede290b
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2024-10-14 11:48:34 +0500

    1

---
+++ BPLA/practice.py
@@ -0,0 +1,89 @@
+# 1 Переменные
+# a = 10
+# print (a)
+
+# 2 Изменение переменной
+# a = 10
+# print(a)
+# a = 20
+# print(a)
+
+# 3 Использование нескольких переменных
+# a = 15
+# b= 5
+# c = a + b
+# print(c)
+
+# 4 Типы данных
+# x = 5
+# y = 3.14
+# text = "hello"
+# is_active = True
+# print (type(x))
+# print (type(y))
+# print (type(text))
+# print (type(is_active))
+
+# 5 Операции
+# a = 10
+# b = 3
+# add = a + b
+# sub = a - b
+# mult = a * b
+# div = a / b
+# f_div = a // b  # целочисленное диление
+# mod = a % b     # остаток от деления
+# exp = a ** b    # возведение в степень
+# print (mod)
+# print (exp) 
+# print (f_div)
+# print (div)
+# print (mult)
+# print (sub)
+# print (add)
+
+# 6 Операторы сравнения
+# a = 10
+# b = 3
+# print (a==b)   # равно или нет
+# print (a != b) # не равно
+# print (a > b)
+# print (a <= b)
+
+# 7 Логические операции
+# a = True
+# b = False
+# print (a and b)  # условие а и б соответствуют
+# print (a or b)   # одно условие если выполнилось
+# print (not a)    # инверсия ответа
+
+# 8 Булевы значения в арифметике
+# a = True
+# b = False
+# result = a + b
+# print (result)
+
+# 9 Сравнение целых чисел с числами с плпавающей точкой
+# a = 5
+# b = 5.0
+# result = a == b
+# print (result)
+
+# 10 Арифметика с целыми и числами с плавающей точкой
+# a = 4
+# b = 2.5
+# result = a + b
+# print (type(result))
+# print (result) 
+
+# 11 Преоброзование типа данных
+# a = 10
+# print (type(a))
+# b = str(a)
+# print (type(b))
+
+# 12 Преоброзание строки в число
+a = "15"
+print (type (a))
+b = int(a)
+print (type (b))
