f7705459bfc4136fc9ca3bc1195689e170b96027
Eduard <115062813+Eduard-Gindullin@users.noreply.github.com>
2024-10-16 13:46:11 +0500

    1

---
+++ BPLA/lection1.py
@@ -0,0 +1,28 @@
+# import math
+
+# print(math.sin)
+
+# from math import sqrt
+
+# print(sqrt(9))
+
+
+# import datetime as dt
+
+# print (dt.datetime.now())
+
+
+# import os
+# current_directory = os.getcwd()
+# print(current_directory)
+
+# import requests
+# url = "https://dzen.ru/"
+# response =requests.get(url)
+# data = response.text
+# print(data)
+
+# def student(name, age):
+#     print(f"Имя: {name} Возраст: {age}")
+
+# student (age = 16, name = "Maria")
